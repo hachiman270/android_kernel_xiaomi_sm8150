fecbbac77f53cee97330dcb99326c89685351453
John Galt <johngaltfirstrun@gmail.com>
2022-05-12 21:15:00 -0400

    cpuset: uclamp_assist: tune based on extensive observation
    These tunings don't benefit any kind of interactive result but do
    increase power consumption in most cases.
    (cherry picked from commit 14e08f5e1c87c1ffac3f611390ba0bf059e0ca96)

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2032,13 +2032,13 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",    	     	"10", "100", 1, 1},
-		{"foreground", 	     	"0",  "50",  0, 0},
-		{"background", 	     	"20", "100", 0, 0},
-		{"system-background", 	"0",  "40",  0, 0},
-		{"camera-daemon",	"50", "100", 1, 1},
-		{"display",		"20", "100", 1, 0},
-		{"restricted",		"0",  "40",  0, 0},
+		{"top-app",    	     	"10", "100", 1, 0},  // 10-100%
+		{"foreground", 	     	"10",  "50",  1, 0},  // 10-50%
+		{"background", 	     	"20", "100", 0, 0},  // 20-100%
+		{"system-background", 	"0",  "40",  0, 0},  // 0-40%
+		{"camera-daemon",	"10", "100", 1, 1},  // 10-100%
+		{"display",		"20", "100", 1, 0},  // 20-100%
+		{"restricted",		"0",  "40",  0, 0},  // 0-40%
 	};
 
 	for (i = 0; i < ARRAY_SIZE(tgts); i++) {
